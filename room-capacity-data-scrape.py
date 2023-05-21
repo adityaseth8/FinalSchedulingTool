2569d57d3fbc77ac575e4c6bf31a3b98c77414dc
adityaseth8 <aseth@ucdavis.edu>
2023-05-21 04:34:19 +0100

    test

--- room-capacity-data-scrape.py
+++ room-capacity-data-scrape.py
@@ -20,3 +20,15 @@ rooms = pd.DataFrame(data[1:], columns=data[0])
 rooms['Capacity'] = pd.to_numeric(rooms['Capacity'])  
 rooms = rooms.sort_values('Capacity', ascending=False)
 print(rooms)
+
+# HTML format
+
+# <div class="_user_table">
+#    <table style="WIDTH:85%;">
+#       <tbody> 
+#          <tr> 
+#              <td> 204 Art</td>
+#              <td> 174 </td>
+#              <td> hhdhd</td>
+#              <td> hhdhfdf </td>
+#          </tr>
