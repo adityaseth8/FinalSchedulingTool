426e75176b5e35aa6d80b85050b68c07fda4a579
Brian Li <brianli4824@gmail.com>
2023-05-21 01:50:01 -0700

    scheduling algo

--- Scheduling-Algo/class_dependencies_graph.py
+++ Scheduling-Algo/class_dependencies_graph.py
@@ -1,13 +1,21 @@
 class AcyclicTopologicalGraph:
     def __init__(self):
         self.graph = {}
+        self.graph_loc = {}
         self.first_node = None
-        self.id = 0
 
-    def print_id(self):
-      print(self.id)
+    def set_location(self, node, location):
+        # self.graph_loc[0]['location'] = location
+        self.graph_loc[node]['location'] = location
 
-    def insert(self, node, department, course_number, class_size, professor, *dependencies):
+    def get_graph(self):
+        return self.graph
+    
+    def get_max_course_size(self):
+        max_size = max(data['class_size'] for data in self.graph.values())
+        return max_size
+
+    def insert(self, node, department, course_number, class_size, professor, *dependencies, location):
         if node in self.graph:
             print(f"Node '{node}' already exists in the graph.")
             return
@@ -20,37 +28,42 @@ class AcyclicTopologicalGraph:
             'course_number': course_number,
             'class_size': class_size,
             'professor': professor,
-            'dependencies': list(dependencies)
+            'dependencies': list(dependencies),
+            'location': location
         }
 
-        if self.has_cyclic_dependencies(node):
-            del self.graph[node]
-            print(f"Inserting node '{node}' creates a cyclic dependency. Node removed.")
-            return
+        # if self.has_cyclic_dependencies(node):
+        #     del self.graph[node]
+        #     print(f"Inserting node '{node}' creates a cyclic dependency. Node removed.")
+        #     return
 
         print(f"Node '{node}' inserted successfully.")
 
-    def has_cyclic_dependencies(self, node, visited=None, stack=None):
-        if visited is None:
-            visited = set()
-        if stack is None:
-            stack = set()
+    # def has_cyclic_dependencies(self, node, visited=None, stack=None):
+    #     if visited is None:
+    #         visited = set()
+    #     if stack is None:
+    #         stack = set()
 
-        visited.add(node)
-        stack.add(node)
+    #     visited.add(node)
+    #     stack.add(node)
 
-        for dependency in self.graph[node]['dependencies']:
-            if dependency not in visited:
-                if self.has_cyclic_dependencies(dependency, visited, stack):
-                    return True
-            elif dependency in stack:
-                return True
+    #     for dependency in self.graph[node]['dependencies']:
+    #         if dependency not in visited:
+    #             if self.has_cyclic_dependencies(dependency, visited, stack):
+    #                 return True
+    #         elif dependency in stack:
+    #             return True
 
-        stack.remove(node)
-        return False
+    #     stack.remove(node)
+    #     return False
 
     def print_nodes(self):
         print(self.graph.items())
+
+    def print_nodes_loc(self):
+        # print(self.loc_list)
+        print(self.graph_loc.items())
 
     def print_nodes_by_criteria(self, criteria, value):
         matching_nodes = [node for node, data in self.graph.items() if data.get(criteria) == value]
@@ -60,6 +73,7 @@ class AcyclicTopologicalGraph:
             print(f"Course Number: {self.graph[node]['course_number']}")
             print(f"Professor: {self.graph[node]['professor']}")
             print(f"Class Size: {self.graph[node]['class_size']}")
+            print(f"Location: {self.graph[node]['location']}")
             print()
       
     def print_nodes_without_edges(self):
@@ -70,54 +84,88 @@ class AcyclicTopologicalGraph:
             print(f"Course Number: {self.graph[node]['course_number']}")
             print(f"Professor: {self.graph[node]['professor']}")
             print(f"Class Size: {self.graph[node]['class_size']}")
+            print(f"Location: {self.graph[node]['location']}")
             print()
 
-def remove_node_with_max_class_size(graph):
-    # Check if valid graph
-    if not graph.graph:
-        print("The graph is empty.")
-        return
+    def remove_node_with_max_class_size(self):
+        # Check if valid graph
+        if len(self.graph) == 0:
+            print("The graph is empty.")
+            exit("Not enough classes to schedule")
+
+        # "Delete" the node with the max class size from the graph
+        sorted_nodes = sorted(self.graph.items(), key=lambda x: x[1]['class_size'], reverse=True)
+        max_class_size_node, max_class_size_data = sorted_nodes[0]
+
+        # Append the deleted node into graph_loc
+        # self.loc_list.append(sorted_nodes[0])
+
+        self.graph_loc.update({max_class_size_node: max_class_size_data})
+
+        # Delete the node from the dependency node 
+        del self.graph[max_class_size_node]
+
+        # Remove all dependencies of the "deleted" node
+        for node, data in self.graph.items():
+            dependencies = data['dependencies']
+            if max_class_size_node in dependencies:
+                # self.graph[node].dependencies.remove(max_class_size_node)
+                self.graph[node]['dependencies'].remove(max_class_size_node)
+
+        print(f"Node '{max_class_size_node}' with the maximum class size {max_class_size_data['class_size']} removed from the graph.")
     
-    # Delete the node with the max class size from the graph
-    sorted_nodes = sorted(graph.graph.items(), key=lambda x: x[1]['class_size'], reverse=True)
-    max_class_size_node, max_class_size_data = sorted_nodes[0]
-    del graph.graph[max_class_size_node]
-
-    # Remove all dependencies of the 
-    for node, data in graph.graph.items():
-        dependencies = data['dependencies']
-        if max_class_size_node in dependencies:
-          dependencies.remove(max_class_size_node)
-
-    print(f"Node '{max_class_size_node}' with the maximum class size {max_class_size_data['class_size']} removed from the graph.")
-
-# finals = []
-# num_days = 5
-# num_blocks_per_day = 5
-# counter = 0
-
-# graph = AcyclicTopologicalGraph()
-
-# for i in range(num_days):
-#   for j in range(num_blocks_per_day):
-#     letter = chr(ord('a') + j)
-#     graph.id = str(i + 1) + letter
-#     finals.append(graph)
-#     finals[counter + i - 1].print_id()  # Check id of graph elements 
+        return sorted_nodes[0]
     
-# finals[0].insert("A", "Math", "101", 300, "Dr. Smith")
-# finals[0].print_id()
+        
+# def remove_node_with_max_class_size(graph):
+#     # Check if valid graph
+#     if len(graph.graph) == 0:
+#         print("The graph is empty.")
+#         exit("Not enough classes to schedule")
+    
+#     # "Delete" the node with the max class size from the graph
+#     sorted_nodes = sorted(graph.graph.items(), key=lambda x: x[1]['class_size'], reverse=True)
+#     max_class_size_node, max_class_size_data = sorted_nodes[0]
+#     # Set the location of deleted node
+#     # del graph.graph[max_class_size_node]
 
-# graph.insert("A", "Math", "101", 300, "Dr. Smith")
-# graph.insert("B", "Physics", "201", 50, "Dr. Johnson", "A")
-# graph.insert("C", "Math", "201", 50, "Dr. Smith", "A")
-# graph.insert("D", "Chemistry", "201", 200, "Dr. Smith", "A")
-# graph.insert("E", "Biology", "201", 100, "Dr. Adams", "D")
+#     # Remove all dependencies of the "deleted" node
+#     for node, data in graph.graph.items():
+#         dependencies = data['dependencies']
+#         if max_class_size_node in dependencies:
+#           dependencies.remove(max_class_size_node)
 
-# graph.print_nodes()
-# graph.print_nodes_without_edges()
-# graph.print_nodes_by_criteria("department", "Math")
+#     print(f"Node '{max_class_size_node}' with the maximum class size {max_class_size_data['class_size']} removed from the graph.")
+    
+#     return sorted_nodes[0]
 
-# remove_node_with_max_class_size(graph)
+# # finals = []
+# # num_days = 5
+# # num_blocks_per_day = 5
+# # counter = 0
 
-# graph.print_nodes()
+# # graph = AcyclicTopologicalGraph()
+
+# # for i in range(num_days):
+# #   for j in range(num_blocks_per_day):
+# #     letter = chr(ord('a') + j)
+# #     graph.id = str(i + 1) + letter
+# #     finals.append(graph)
+# #     finals[counter + i - 1].print_id()  # Check id of graph elements 
+    
+# # finals[0].insert("A", "Math", "101", 300, "Dr. Smith")
+# # finals[0].print_id()
+
+# # graph.insert("A", "Math", "101", 300, "Dr. Smith")
+# # graph.insert("B", "Physics", "201", 50, "Dr. Johnson", "A")
+# # graph.insert("C", "Math", "201", 50, "Dr. Smith", "A")
+# # graph.insert("D", "Chemistry", "201", 200, "Dr. Smith", "A")
+# # graph.insert("E", "Biology", "201", 100, "Dr. Adams", "D")
+
+# # graph.print_nodes()
+# # graph.print_nodes_without_edges()
+# # graph.print_nodes_by_criteria("department", "Math")
+
+# # remove_node_with_max_class_size(graph)
+
+# # graph.print_nodes()
--- Scheduling-Algo/main.py
+++ Scheduling-Algo/main.py
@@ -1,23 +1,103 @@
 from class_dependencies_graph import AcyclicTopologicalGraph
-from room_capacity_data_scrape import RoomCapacity
+import pandas as pd 
+
 
 finals = []
-num_days = 5
+num_days = 4
 num_blocks_per_day = 5
 counter = 0
 
-graph = AcyclicTopologicalGraph()
-room_data = RoomCapacity()
+# graph = AcyclicTopologicalGraph()
+room_df = pd.read_csv('data.csv')
 
+# Assign ids to all graphs in each block per day 
 for i in range(num_days):
   for j in range(num_blocks_per_day):
+    # Create object instance
+    graph = AcyclicTopologicalGraph()
+
+    # Increment letter and attach to graph id
     letter = chr(ord('a') + j)
     graph.id = str(i + 1) + letter
+
+    # Append graph to finals list 
     finals.append(graph)
     # finals[counter + i - 1].print_id()  # Check id of graph elements 
 
-finals[0].insert("A", "Math", "101", 300, "Dr. Smith")
-finals[0].insert("B", "Physics", "201", 50, "Dr. Johnson", "A")
-finals[0].insert("C", "Math", "201", 50, "Dr. Smith", "B")
+# Insert the classes that need to be scheduled in each block
+finals[0].insert("A", "Math", "101", 300, "Dr. Smith", None, location="")
+finals[0].insert("B", "Physics", "201", 50, "Dr. Johnson", "A", location="")
+finals[0].insert("C", "Math", "201", 50, "Dr. Smith", "B", location="")
+
+finals[1].insert(1, "A", "Math", "101", 300, "Dr. Smith", None, location="")
+finals[1].insert(2, "B", "Physics", "201", 50, "Dr. Johnson", "A", location="")
+finals[1].insert(3, "C", "Math", "201", 50, "Dr. Smith", "B", location="")
+
+# # Scoped down code to fit classes to rooms 
+# num_classes_to_schedule = 1 # len(finals[0].graph)
+
+for i in range(num_days * num_blocks_per_day):
+  num_classes_to_schedule = len(finals[i].graph)
+  print(num_classes_to_schedule)
+  if (num_classes_to_schedule == 0): break
+
+  for j in range(num_classes_to_schedule):
+    # Get max class size 
+    max_course_size = finals[i].get_max_course_size()
+    # print(max_course_size)
+
+    # Get max room size
+    max_room_size = room_df["Capacity"].max()
+    # print(int(max_room_size))
+
+    # Assign the room with the higheset class size
+    max_room_size_name = room_df.loc[room_df['Capacity'].idxmax(), 'Classroom List']
+
+    # If first column with max capacity can fit class size, can fit class
+    if (int(max_room_size) > int(max_course_size)):
+      # Assign the room with the higheset class size
+      location = room_df.loc[room_df['Capacity'].idxmax(), 'Classroom List']
+
+      # Get the node and data of the deleted node 
+      # Appends deleted node in graph_loc
+      (max_class_size_node, max_class_size_data) = finals[i].remove_node_with_max_class_size()
+
+      # Assign the location to graph storing nodes with locations
+      finals[i].set_location(max_class_size_node, location)
+
+      # Re-define data frame with the first row
+      room_df = room_df.iloc[1:]
+  
+  finals[i].print_nodes_loc()
+
+# finals[0].print_nodes()
+
+# Generalized 
+# for i in range(num_days * num_blocks_per_day):
+#   num_classes_to_schedule = len(finals[i].graph)
+#   print(num_classes_to_schedule)
+#   if (num_classes_to_schedule == 0): break
+
+#   for j in range(num_classes_to_schedule):
+#       # Get the node and data of the deleted node 
+#       max_class_size_node, max_class_size_data = remove_node_with_max_class_size(finals[j])
+      
+#       max_class_size = max_class_size_data['class_size']
+#       max_room_size = room_df["Capacity"].max()
+#       # print(max_class_size, max_room_size)
+#       # max_room_size = room_df.at[0, "Capacity"]
+
+#     # If first column with max capacity can fit class size, can fit class
+#       if (max_room_size >= max_class_size):
+#         # Assign the room with the higheset class size
+#         location = room_df.loc[room_df['Capacity'].idxmax(), 'Classroom List']
+
+#         # Assign the location to the node 
+#         finals[j].graph[max_class_size_node]['location'] = location
+#         print(finals[j].print_nodes())
+
+#         # Rededine data frame with the first row
+#         room_df = room_df.iloc[1:]
+
 
 
